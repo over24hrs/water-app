c64e5dcd10b8cb18f8f293f8718a6650d1dd84c8
over24hrs <153599939+over24hrs@users.noreply.github.com>
2026-05-11 14:22:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
 // config.js
-// ⚠️ เปลี่ยน URL นี้เป็น Web App URL ของคุณ
 const GAS_URL = 'https://script.google.com/macros/s/AKfycbzQLCf_NCPrJVKrDuuvYWsfcSb-jakv1QAxrJXKtcbPZ_M1DHUbW8VvuPfnmPI4ynbFZA/exec';
